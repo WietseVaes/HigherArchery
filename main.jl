57ebe4593ec2b9d5f4dff9e1eec4c9c9fc0ebb5c
kaillahs <kaillahs@uw.edu>
2025-02-05 12:28:22 -0800

    main

--- main.jl
+++ main.jl
@@ -26,16 +26,17 @@ U, V, Adiag = HODLR(A, 50);
 A_HODLR_paper = HODLR_revert(U, V, Adiag)
 #
 A_HBS, Ut, _, _, _, _ = HBS(A, 50)
-#A_HBS_ID = HBS_ID(A, 50)
+A_HBS_ID = HBS_ID(A, 50)
 
 # Error displays
 @printf "my HODLR: rel. normed err: \n" 
 (norm(A_HODLR - A) / norm(A))|>display
 @printf "HODLR: rel. normed err."
 (norm(A_HODLR_paper - A) / norm(A))|>display
-@printf "HBS: rel. normed err. %.5f\n" 
+@printf "HBS: rel. normed err:\n" 
 (norm(A_HBS - A) / norm(A))|>display
-#@printf "HBS ID: rel. normed err. %.5f\n" (norm(A_HBS_ID - A) / norm(A))
+@printf "HBS ID: rel. normed err:\n" 
+(norm(A_HBS_ID - A) / norm(A))|>display
 
 # Plots
 plotlyjs()  # Or use your preferred backend
@@ -44,23 +45,22 @@ p1 = heatmap(abs.(A_HODLR - A), color=:jet, zscale=:log10)
 xlims!(1, size(A, 1))
 ylims!(1, size(A, 2))
 
-p2 = surface(abs.(A_HODLR_paper - A), color=:jet, zscale=:log10,
+p2 = heatmap(abs.(A_HODLR_paper - A), color=:jet, zscale=:log10,
              title=@sprintf("HODLR: rel. normed err. %.5f", norm(A_HODLR_paper - A) / norm(A)))
 xlims!(1, size(A, 1))
 ylims!(1, size(A, 2))
 
-p3 = surface(abs.(A_HBS - A), color=:jet, zscale=:log10,
+p3 = heatmap(abs.(A_HBS - A), color=:jet, zscale=:log10,
              title=@sprintf("HBS: rel. normed err. %.5f", norm(A_HBS - A) / norm(A)))
 xlims!(1, size(A, 1))
 ylims!(1, size(A, 2))
 
-#p4 = surface(abs.(A_HBS_ID - A), color=:jet, zscale=:log10,
-             #title=@sprintf("HBS ID: rel. normed err. %.5f", norm(A_HBS_ID - A) / norm(A)))
-#xlims!(1, size(A, 1))
-#ylims!(1, size(A, 2))
+p4 = heatmap(abs.(A_HBS_ID - A), color=:jet, zscale=:log10,
+             title=@sprintf("HBS ID: rel. normed err. %.5f", norm(A_HBS_ID - A) / norm(A)))
+xlims!(1, size(A, 1))
+ylims!(1, size(A, 2))
 
 # Combine subplots
-#plot(p1, p2, p3, p4, layout=(2, 2), size=(1000, 800))
-plot(p1, p2, p3, layout=(2, 2), size=(1000, 800))
+plot(p1, p2, p3, p4, layout=(2, 2), size=(1000, 800))
 
 #
